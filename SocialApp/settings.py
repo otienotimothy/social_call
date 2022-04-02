ba0e765c8ef123b10cc35ba0289f7f46e351003b
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-02 10:35:16 +0300

    Add the social app to the app configurations in project settings

--- SocialApp/settings.py
+++ SocialApp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Created Apps
+    'social.apps.SocialConfig'
 ]
 
 MIDDLEWARE = [

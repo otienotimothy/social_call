5911953c56028e352cd7bdc2054b75a8bcee5015
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-02 10:44:36 +0300

    Define view functions for the landing page and the home page

--- social/views.py
+++ social/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return 'Hello From the Landing Page(index)'
+
+def home(request):
+    return 'Hello From the Home Page'

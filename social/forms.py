746bd2eb5718815c296bd20938d5e223589e5f07
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-03 19:36:56 +0300

    Add class definition for User creation and login forms

---
+++ social/forms.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.forms import EmailInput, TextInput, PasswordInput
+
+class UserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2' ]
+        widgets = {
+            'username': TextInput(attrs={'class': 'form-control'}),
+            'email': EmailInput(attrs={'class': 'form-control'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+
+class LoginUserForm(forms.Form):
+
+    username = forms.CharField(widget=TextInput(
+        attrs={'class': 'form-control'}), max_length=100)
+    password = forms.CharField(widget=PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget.attrs['class'] = 'form-control'
